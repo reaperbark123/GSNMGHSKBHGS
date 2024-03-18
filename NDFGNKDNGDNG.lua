6768cb3710667a22a1edb2262a510f9ec1fc6967
reaperbark123 <88252453+reaperbark123@users.noreply.github.com>
2024-03-18 18:33:57 -0500

    Create NDFGNKDNGDNG.lua

---
+++ NDFGNKDNGDNG.lua
@@ -0,0 +1,23 @@
+if ExecutedAlready then
+    warn("[Reaper's Hub]: has already executed.")
+    return
+end
+getgenv().ExecutedAlready = true;
+
+local Id = game.GameId;
+
+if Id == 3057370181 then -- RB World 4
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dbc9a4b9b5af985150fa110523f02a83.lua"))()
+elseif Id == 4965021724 then -- Hoop Journey
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7f97ce2f84c8d45793196132a522b83d.lua"))()
+elseif Id == 2459091562 then -- RH2
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/391684a0b738a6f2523ca9935a5e7097.lua"))()
+else
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b02b433bbccc5fc5e5262dc11c548d36.lua"))() -- Default Loader (All other games)
+end;
+
+-- elseif Id == 00 then
+    
+-- elseif Id == 00 then
+    
+-- elseif Id == 00 then
