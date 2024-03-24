f9b5accb799fce5b5ea730d72ced4cb46d6d5264
reaperbark123 <88252453+reaperbark123@users.noreply.github.com>
2024-03-24 16:29:04 -0500

    Update NDFGNKDNGDNG.lua

--- NDFGNKDNGDNG.lua
+++ NDFGNKDNGDNG.lua
@@ -8,13 +8,13 @@ getgenv().ExecutedAlready = true;
 local Id = game.GameId;
 
 if Id == 3057370181 then -- RB World 4
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dbc9a4b9b5af985150fa110523f02a83.lua"))()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f0af5fcb4936e0aee800387bee35d7d3.lua"))()
 elseif Id == 4965021724 then -- Hoop Journey
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7f97ce2f84c8d45793196132a522b83d.lua"))()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dfb5631de08aa67c3280903385705144.lua"))()
 elseif Id == 2459091562 then -- RH2
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/391684a0b738a6f2523ca9935a5e7097.lua"))()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/623a825ee75f2f45a5afae114f183c9b.lua"))()
 else
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b02b433bbccc5fc5e5262dc11c548d36.lua"))() -- Default Loader (All other games)
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5d9c8257eee9b4a8f2dc58b1fc633c1f.lua"))()
 end;
 
 -- elseif Id == 00 then
