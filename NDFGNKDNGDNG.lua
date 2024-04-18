117cf587fa4fcd2c76f5e8173c968e97d96ea194
reaperbark123 <88252453+reaperbark123@users.noreply.github.com>
2024-04-17 20:31:18 -0500

    Update NDFGNKDNGDNG.lua

--- NDFGNKDNGDNG.lua
+++ NDFGNKDNGDNG.lua
@@ -9,8 +9,8 @@ local Id = game.GameId;
 
 if Id == 3057370181 then -- RB World 4
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f0af5fcb4936e0aee800387bee35d7d3.lua"))()
-elseif Id == 4965021724 then -- Hoop Journey
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dfb5631de08aa67c3280903385705144.lua"))()
+elseif Id == 5370286620 then -- Hoop Nations
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/94a82415a5e9a1629c1f6e53827f227d.lua"))()
 elseif Id == 2459091562 then -- RH2
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/623a825ee75f2f45a5afae114f183c9b.lua"))()
 else
