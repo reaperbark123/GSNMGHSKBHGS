ed2b1f5854bed69faddf2ff69db88e08990fc484
reaperbark123 <88252453+reaperbark123@users.noreply.github.com>
2025-02-01 00:08:19 -0600

    Update NDFGNKDNGDNG.lua

--- NDFGNKDNGDNG.lua
+++ NDFGNKDNGDNG.lua
@@ -13,6 +13,8 @@ elseif Id == 5370286620 then -- Hoop Nations
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/94a82415a5e9a1629c1f6e53827f227d.lua"))()
 elseif Id == 2459091562 then -- RH2
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/623a825ee75f2f45a5afae114f183c9b.lua"))()
+elseif Id == 5462326700 then -- RB World 5
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dcf631cdf048baec0e03d0f2f56c5e09.lua"))()
 else
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5d9c8257eee9b4a8f2dc58b1fc633c1f.lua"))()
 end;
